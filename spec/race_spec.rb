3ab27866ae3557fd4663ca7481c1ce68ac21505a
Svetoslav Blyahov <svetoslav@tradeo.com>
2017-07-16 16:54:43 +0300

    Refactor tests

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -1,80 +1,95 @@
 require_relative 'spec_helper'
 require_relative '../lib/race'
 
-RSpec.describe RaceBet::Race do # rubocop:disable Metrics/BlockLength
+# rubocop:disable Metrics/BlockLength
+RSpec.describe RaceBet::Race do
   let(:guesses) { %i(joe mary bob sheldon howard frank) }
+  let(:first) { RaceBet::Race::SCORING[:exact_hit][0] }
+  let(:second) { RaceBet::Race::SCORING[:exact_hit][1] }
+  let(:third) { RaceBet::Race::SCORING[:exact_hit][2] }
+  let(:fourth) { RaceBet::Race::SCORING[:exact_hit][3] }
+  let(:fifth) { RaceBet::Race::SCORING[:exact_hit][4] }
+  let(:misplaced) { RaceBet::Race::SCORING[:top_5] }
 
-  subject { described_class.score(guesses, winners) }
-
-  context 'all guesses correct' do
-    let(:guesses) { %i(joe mary bob) }
-    let(:winners) { guesses }
-
-    it 'calculates 15, 10, 5 points respectively for correct guesses' do
-      expect(subject).to eq(15 + 10 + 5)
+  def winner_place(n)
+    (1..n + 1).to_a.map do |place|
+      n != place ? "loser#{place}" : guesses[n - 1]
     end
   end
 
-  context 'no points for no correct guesses' do
-    let(:winners) { %i(loser loser1) }
+  describe '#score' do
+    subject { described_class.score(guesses, winners) }
 
-    it { expect(subject).to eq(0) }
-  end
+    context 'all guesses correct' do
+      let(:guesses) { %i(joe mary bob) }
+      let(:winners) { guesses }
 
-  context '15 points for first place' do
-    let(:winners) { [guesses[0], :loser, :loser2] }
-
-    it { expect(subject).to eq(15) }
-  end
-
-  context '10 points for second place' do
-    let(:winners) { [:loser, guesses[1], :loser2] }
-
-    it { expect(subject).to eq(10) }
-  end
-
-  context '5 points for third place' do
-    let(:winners) { [:loser, :loser2, guesses[2]] }
-
-    it { expect(subject).to eq(5) }
-  end
-
-  context '3 points for fourth place' do
-    let(:winners) { [:loser, :loser2, :loser3, guesses[3]] }
-
-    it { expect(subject).to eq(3) }
-  end
-
-  context '1 point for fifth place' do
-    let(:winners) { [:loser, :loser2, :loser3, :loser4, guesses[4]] }
-
-    it { expect(subject).to eq(1) }
-  end
-
-  context 'gives one point for a correct guess in the wrong place' do
-    let(:winners) do
-      winners = [guesses[0], :loser, :loser2]
-      winners.unshift(winners.pop)
-      winners
+      it 'calculates points respectively for correct guesses' do
+        expect(subject).to eq(first + second + third)
+      end
     end
 
-    it { expect(subject).to eq(1) }
-  end
+    context 'no points for no correct guesses' do
+      let(:winners) { %i(loser loser1) }
 
-  context 'scores positional and misplaced guesses at the same time' do
-    let(:winners) do
-      winners = [guesses[0], :loser, :loser2, guesses[3], :loser3]
-      winners[3], winners[4] = winners[4], winners[3]
-      winners
+      it { expect(subject).to eq(0) }
     end
 
-    it { expect(subject).to eq(15 + 1) }
-  end
+    context '15 points for first place' do
+      let(:winners) { winner_place(1) }
 
-  context 'README example' do
-    let(:guesses) { %i(bob mark walter) }
-    let(:winners) { %i(mark bob walter) }
+      it { expect(subject).to eq(first) }
+    end
 
-    it { expect(subject).to eq(5 + 1 + 1) }
+    context '10 points for second place' do
+      let(:winners) { winner_place(2) }
+
+      it { expect(subject).to eq(second) }
+    end
+
+    context '5 points for third place' do
+      let(:winners) { winner_place(3) }
+
+      it { expect(subject).to eq(third) }
+    end
+
+    context '3 points for fourth place' do
+      let(:winners) { winner_place(4) }
+
+      it { expect(subject).to eq(fourth) }
+    end
+
+    context '1 point for fifth place' do
+      let(:winners) { winner_place(5) }
+
+      it { expect(subject).to eq(fifth) }
+    end
+
+    context 'gives one point for a correct guess in the wrong place' do
+      let(:winners) do
+        winners = winner_place(1)
+        winners.unshift(winners.pop)
+      end
+
+      it { expect(subject).to eq(misplaced) }
+    end
+
+    context 'scores positional and misplaced guesses at the same time' do
+      let(:winners) do
+        winners = [guesses[0], :loser, :loser2, guesses[3], :loser3]
+        winners[3], winners[4] = winners[4], winners[3]
+        winners
+      end
+
+      it { expect(subject).to eq(first + misplaced) }
+    end
+
+    context 'README example' do
+      let(:guesses) { %i(bob mark walter) }
+      let(:winners) { %i(mark bob walter) }
+
+      it { expect(subject).to eq(third + 2 * misplaced) }
+    end
   end
+  # rubocop:enable Metrics/BlockLength
 end
